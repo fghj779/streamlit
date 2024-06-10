8e9b66ec53ae962dc71572acb5ecc46802d1dbe0
ayapechi <112924797+ayapechi@users.noreply.github.com>
2024-06-10 16:56:58 +0900

    Update 기말과제.py

--- 기말과제.py
+++ 기말과제.py
@@ -2,11 +2,9 @@ import streamlit as st
 import pandas as pd
 import gspread
 from datetime import datetime, timedelta
-import gspread
 from oauth2client.service_account import ServiceAccountCredentials
 
-
-# Google Sheets認証設定
+# Google Sheets 인증 설정
 scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
 json_keyfile = 'C:/Users/USER/Desktop/4-1/info-prog-adv-2024/in-02-first-trial/week15/golden-index-424607-e1-504dc484c349.json'
 
@@ -14,14 +12,8 @@ creds = ServiceAccountCredentials.from_json_keyfile_name(json_keyfile, scope)
 
 client = gspread.authorize(creds)
 
-# Google Sheetsに接続
-print(1111)
-# doc = client.open("finalfinal")
-# print(doc)
-# sheet = doc.worksheet('sheet1')
-# print(sheet)
+# Google Sheets에 연결
 sheet = client.open("finalfinal").sheet1
-
 
 # Streamlit을 사용한 입력 폼
 st.title("가계부 입력")
@@ -59,4 +51,3 @@ if not df.empty:
     st.line_chart(weekly_summary.set_index('주'))
 else:
     st.write("데이터가 없습니다.")
-
